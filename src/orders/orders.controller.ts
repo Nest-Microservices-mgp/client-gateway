f16a4a79d252b61c04ae75c69275a201fe8afcb2
mgarces <manugp1994@gmail.com>
2025-08-01 16:10:25 +0200

    Video62

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -10,9 +10,9 @@ import {
 } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
+import { PaginationDto } from 'src/common';
 import { ORDER_SERVICE } from 'src/config/services';
-import { OrderPaginationDto } from './dto';
-import { CreateOrderDto } from './dto/create-order.dto';
+import { CreateOrderDto, OrderPaginationDto, StatusDto } from './dto';
 
 @Controller('orders')
 export class OrdersController {
@@ -30,13 +30,28 @@ export class OrdersController {
     return this.orderClient.send('findAllOrders', orderPaginationDto);
   }
 
-  @Get(':id')
+  @Get('id/:id')
   async findOne(@Param('id', ParseUUIDPipe) id: string) {
     try {
       const order = await firstValueFrom(
         this.orderClient.send('findOneOrder', { id }),
       );
       return order;
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+  @Get(':status')
+  async findByStatus(
+    @Param() statusDto: StatusDto,
+    @Query() paginationDto: PaginationDto,
+  ) {
+    try {
+      return this.orderClient.send('findAllOrders', {
+        ...paginationDto,
+        status: statusDto.status,
+      });
     } catch (error) {
       throw new RpcException(error);
     }
