8dd061dc8a393caae888c3ca2e10bb2fc5cc4d1f
mgarces <manugp1994@gmail.com>
2025-08-02 11:20:33 +0200

    Video84

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,22 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 
-import { envs, NATS_SERVICE } from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
 import { ProductsController } from './products.controller';
 
 @Module({
   controllers: [ProductsController],
   providers: [],
-  imports: [
-    ClientsModule.register([
-      {
-        name: NATS_SERVICE,
-        transport: Transport.NATS,
-        options: {
-          servers: envs.natsServers,
-        },
-      },
-    ]),
-  ],
+  imports: [NatsModule],
 })
 export class ProductsModule {}
